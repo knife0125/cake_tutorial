97b84d7182aa0eacd370b7019dc868f6264d510a
Daisuke Nishide <d.nishide0125@gmail.com>
2014-10-10 07:23:19 +0800

    Modify security configurations.

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '1c945845acd84b4c337f06744f89e75168779c48');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '907659240454372449283986612696');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
