2f85503b6e1b05517e0efedfdf457bc4cf4cb08f
Daisuke Nishide <d.nishide0125@gmail.com>
2014-10-10 07:58:54 +0800

    記事投稿時にバリデーションが効くように変更

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -1,5 +1,12 @@
 <?php
 
     class Post extends AppModel {
-
+        public $validate = array(
+            'title' => array(
+                'rule' => 'noEmpty'
+            ),
+            'body' => array(
+                'rule' => 'noEmpty'
+            )
+        );
     }
