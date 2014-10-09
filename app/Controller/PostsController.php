6383836e8b8ebb52e20eff3ce4b2f4203ba36848
Daisuke Nishide <d.nishide0125@gmail.com>
2014-10-10 07:56:39 +0800

    記事の投稿(addアクション)が機能するように変更

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -1,6 +1,7 @@
 <?php
     class PostsController extends AppController {
-        public $helpers = array('Html', 'Form');
+        public $helpers = array('Html', 'Form', 'Session');
+        public $components = array('Session');
 
         public function index()
         {
@@ -23,4 +24,14 @@
             $this->set('post', $post);
         }
 
+        public function add() {
+            if ($this->request->is('post')) {
+                $this->Post->create();
+                if ($this->Post->save($this->request->data)) {
+                    $this->Session->setFlash(__('Your post has been saved.'));
+                    return $this->redirect(array('action' => 'index'));
+                }
+                $this->Session->setFlash(__('Unable to add your post.'));
+            }
+        }
     }
