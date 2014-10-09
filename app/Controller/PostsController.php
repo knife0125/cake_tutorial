38c1a11be0f18f8bc7341f08ae329b9fcc31cb77
Daisuke Nishide <d.nishide0125@gmail.com>
2014-10-10 07:48:45 +0800

    PostsControllerからPostsView/index.ctpにデータを受け渡すよう変更

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -1,9 +1,14 @@
 <?php
     class PostsController extends AppController {
+        public $helpers = array('Html', 'Form');
 
         public function index()
         {
+            // PostsModelから全てのポストデータを取得
+            $postList = $this->Post->find('all');
 
+            // ビューに取得したポストデータをセット('posts'という名前でビューに渡す)
+            $this->set('posts', $postList);
         }
 
     }
