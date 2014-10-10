10956c324baf620e69510095caf5d0f4a8cc7986
Daisuke Nishide <d.nishide0125@gmail.com>
2014-10-10 08:58:49 +0800

    カテゴリの詳細画面を表示できるように修正

--- app/Controller/CategoriesController.php
+++ app/Controller/CategoriesController.php
@@ -9,4 +9,21 @@
             // ビューに取得したポストデータをセット('posts'という名前でビューに渡す)
             $this->set('categories', $categoryList);
         }
+
+        public function view($categoryId = null)
+        {
+            // view画面描画時にカテゴリidが取得できない場合はエラー表示
+            if (!$categoryId) {
+                throw new NotFoundException(__('Invalid category'));
+            }
+
+            // 詳細表示するカテゴリの情報をModelから取得
+            $category = $this->Category->findById($categoryId);
+            if (!$category) {
+                throw new NotFoundException(__('Invalid category'));
+            }
+
+            // 取得したカテゴリの情報をカテゴリの画面に引き渡す
+            $this->set('category', $category);
+        }
     }
---
+++ app/View/Categories/view.ctp
@@ -0,0 +1,2 @@
+<h1><?php echo h($category['Category']['category_name']);?></h1>
+<p><small>Created: <?php echo $category['Category']['created'];?></small></p>
