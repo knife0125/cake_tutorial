31024d9d84e06f481477fdb7d36ad63853bbb9b6
Daisuke Nishide <d.nishide0125@gmail.com>
2014-10-12 06:09:39 +0800

    データベースリレーションの設定のモデル

--- app/Model/Category.php
+++ app/Model/Category.php
@@ -1,8 +1,29 @@
 <?php
     class Category extends AppModel {
+        public $name = 'Category';
         public $validate = array(
             'category_name' => array(
                 'rule' => 'notEmpty'
             )
         );
+        public $hasMany = array (
+            'Post' => array(
+                'className'     => 'Post',
+                'foreignKey'    => 'category_id',
+                'order'         => 'Post.created ASC',
+            )
+        );
+        public function getPostsWithCategory($categoryId)
+        {
+            // postsデータの初期化
+            $posts = array();
+            // 指定条件の作成
+            $conditions = array(
+                'Category.id' => $categoryId
+            );
+            // 条件を指定してデータを取得
+            $posts = $this->find('all', array('conditions' => $conditions));
+            return $posts;
+        }
     }
+
